8a62b1d509cebe2d1859e8691ddf11c5b5791782
Diego Fernando Salazar Rosero <dfsalazarr@siesa.com>
2025-07-22 11:33:09 -0500

    se sube controladore eliminados por cambio de repositorio

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const app = require("./app");
-
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
